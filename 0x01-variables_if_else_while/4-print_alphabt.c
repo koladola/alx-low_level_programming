6803fdf02fc005e12b8b681b9f10751997b6e787
kolade <koladeolasugba@gmail.com>
2023-03-17 16:30:24 -0700

    using continue

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - testing continu
+ *
+ * Return: o for okay
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
