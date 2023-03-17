3ac613951aad53e20579510c0e075b8af60ca895
kolade <koladeolasugba@gmail.com>
2023-03-16 21:45:31 -0700

    Task puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: return 0 if all is okay
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
