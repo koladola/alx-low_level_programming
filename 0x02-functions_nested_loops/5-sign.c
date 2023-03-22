318faa4de93c125ed078bb542ad166f5218a826e
kolade <koladeolasugba@gmail.com>
2023-03-21 17:06:06 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign - function
+ *
+ * Return: 0
+ *@c: - xter
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
