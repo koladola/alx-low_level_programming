007d05b753d1de55f2fa6cd8f4031fbb56069af8
kolade <koladeolasugba@gmail.com>
2023-03-22 14:40:02 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code
+ *
+ * @q: call var
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int q)
+{
+	int k;
+	
+	if (q < 0)
+                q = -q;
+
+	k = q % 10;
+	 _putchar(k + '0');
+	/* _putchar('\n');
+	*/
+	/* if (k < 0)
+	 * *	k = -k;
+	 */
+	return (k);
+}
