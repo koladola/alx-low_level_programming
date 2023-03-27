65783c5043ca3a10451b4e3aeded22513c92ade0
kolade <koladeolasugba@gmail.com>
2023-03-27 14:48:35 -0700

    Task )2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main_h"
+
+/**
+ * _strlen - check the code
+ *
+ * @s: variable from calling prog
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s != '\0')
+	{
+		count++;
+		S++;
+	}
+	return (count);
+}
