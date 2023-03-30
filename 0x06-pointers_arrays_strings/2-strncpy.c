68a3d054bc0accf8aa3636fa86cebe65216d9a2e
kolade <koladeolasugba@gmail.com>
2023-03-30 15:19:23 -0700

    Task 02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,7 +13,18 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
 		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
 	return (dest);
 }
+
