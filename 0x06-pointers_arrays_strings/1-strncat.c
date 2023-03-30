ebe1a661ab1154b745dcdd40dc25c7f5ac32f93d
kolade <koladeolasugba@gmail.com>
2023-03-30 14:42:25 -0700

    Task 01

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat- check the code
+ *
+ * @dest: destination
+ * @src:source
+ *
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y, j;
+	int destcount = 0;
+	int srccount = 0;
+
+	for (x = 0; dest[x] != '\0'; x++)
+		destcount++;
+	for (y = 0; src[y] != '\0'; y++)
+		srccount++;
+	for (j = 0; j < n; j++)
+		dest[destcount + j] = src[j];
+	return (dest);
+}
