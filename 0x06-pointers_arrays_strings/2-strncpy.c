3f62ce09bd69591f7dfcf0e517bc849f9b0f5e65
kolade <koladeolasugba@gmail.com>
2023-03-30 15:07:38 -0700

    Task 02

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy- check the code
+ *
+ * @dest: destination
+ * @src:source
+ *
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+		dest[j] = src[j];
+	return (dest);
+}
