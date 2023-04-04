06f7ce85c5d1bd81dc97fd6edea3cc8565f85516
kolade <koladeolasugba@gmail.com>
2023-04-04 14:07:37 -0700

    Task 03

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+	int r;
+
+	while (*s != '\0')
+	{
+		for (r = 0; accept[r] != '\0'; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}
+
